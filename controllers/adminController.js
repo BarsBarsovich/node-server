9be41c0f3cefffefcc76593ae52e6517cfda86aa
barspredator <boris.dryabin@gmail.com>
2019-07-01 15:57:48 +0300

    task3 complete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,28 +6,37 @@ const fs = require('fs');
 
 module.exports.get = function (req, res) {
   console.log('Endpoint checked');
-  res.render('pages/admin', { title: 'admin' })
+  res.render('pages/admin', {title: 'admin'})
 }
 
 module.exports.upload = function (req, res, next) {
   try {
-    const { photo, name, price } = req.body;
+    const {photo, name, price} = req.body;
     let form = new formidable.IncomingForm();
     let upload = path.join('public', 'upload');
 
-    // if (!fs.existsSync(upload)) {
-    //   fs.mkdirSync(upload);
-    // }
+    if (!fs.existsSync(upload)) {
+      fs.mkdirSync(upload);
+    }
 
     form.uploadDir = path.join(process.cwd(), upload);
 
     console.log(form.uploadDir);
 
 
-    form.parse(req, function(err, fields, files) {
+    form.parse(req, function (err, fields, files) {
       console.log('Form was parsed');
+      const fileName = path.join(upload, files.photo.name);
+
+      fs.rename(files.photo.path, fileName, (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+      })
+
+
+      res.redirect('/admin?status=Good');
     });
-    
 
   } catch (err) {
     console.error(err);
@@ -36,7 +45,7 @@ module.exports.upload = function (req, res, next) {
 
 
 module.exports.addSkills = function (req, res, next) {
-  const { age, concerts, cities, years } = req.body;
+  const {age, concerts, cities, years} = req.body;
   dbWorker.skillsAdd(age, concerts, cities, years);
   res.redirect(req.get('referer'));
   next();
